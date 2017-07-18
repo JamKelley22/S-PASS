f4533ff567c02422dc9409d82774f355bd39f1dc
Masashi Schafer <masashi@virtualspaces.io>
2017-07-18 14:33:23 -0500

    Merged Masashi for real

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,9 @@ import MasashiRouter from './MasashiTest/MasashiTest.js';
 import PhaseOne from './PhaseOne/PhaseOne.js';
 import SideBar from './SideBar/Sidebar.js';
 import Home from './Home/Home.js';
-<<<<<<< HEAD
 import TopNav from './SideBar/navbar.js';
 import Breadcrumbs from './breadcrumbs.js';
-=======
 import './App.css';
->>>>>>> masashi
 import {PageHeader,Button,Grid,Row,Col,Nav,NavItem} from 'react-bootstrap';
 
 
@@ -19,29 +16,19 @@ const App = () => (
     <PageHeader>
       <TopNav/>
     </PageHeader>
-<<<<<<< HEAD
     <Breadcrumbs/>
     <Row>
-=======
-    <Row className="test">
->>>>>>> masashi
       <Col xs={3} md={3}>
         <SideBar/>
       </Col>
       <Col xs={9} md={9}>
-<<<<<<< HEAD
-        <Route exact path="/" component={Home}/>
-        <Route path="/Layout" component={Layout}/>
-        <Route path="/MasashiRouter" component={MasashiRouter}/>
-        <Route path="/Home" component={Home}/>
-=======
         <div className="pre-scrollable">
           <Route exact path="/" component={Home}/>
           <Route path="/Layout" component={Layout}/>
           <Route path="/MasashiRouter" component={MasashiRouter}/>
+          <Route path="/Home" component={Home}/>
           <Route path="/PhaseOne" component={PhaseOne}/>
         </div>
->>>>>>> masashi
       </Col>
     </Row>
   </Grid>
--- src/components/MasashiTest/MasashiTest.js
+++ src/components/MasashiTest/MasashiTest.js
@@ -7,11 +7,7 @@ const MasashiRouter = ({match}) =>(
       <div>
         <h1>
           <Link to={`${match.url}/MasashiTables`}>
-<<<<<<< HEAD
-            <Button bsStyle='danger'>Don'\''t Click Me</Button>
-=======
             <Button bsStyle='danger'>Do not Click Me</Button>
->>>>>>> masashi
           </Link>
         </h1>
          <Route path={`${match.url}/MasashiTables`} component={MasashiTables}/>
