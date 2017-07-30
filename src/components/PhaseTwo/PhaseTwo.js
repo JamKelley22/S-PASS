329c787f9eed7dcaee277171164ad6593e3b6460
Masashi Schafer <masashi@virtualspaces.io>
2017-07-30 16:22:02 -0500

    Fixed output of phase one. this.math.multiply() input was was a matrix instead of 2d array

--- src/components/PhaseTwo/PhaseTwo.js
+++ src/components/PhaseTwo/PhaseTwo.js
@@ -16,70 +16,14 @@ export default class PhaseTwo extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      value: '1',
-      stations: [
-        ['station one','000','453'],
-        ['station two','001'],
-        ['station two','001'],
-        ['station two','001']
-      ]
-    };
-    this.something = this.something.bind(this);
-  }
-
-  something(name,i,j){
-    var person = prompt("Please enter the function name:", "Function");
-    var s2 = this.state.stations;
-    s2[i][j] = person;
-    this.setState({stations: s2});
-  }
-
-  getInitialState() {
-    return {
-      value: '1',
-      stations: [
-        ['station one','000','453'],
-        ['station two','001'],
-        ['station two','001'],
-        ['station two','001']
-      ]
     };
   }
-
 
   render(){
 
-    var something = this.something;
-
-    var users = this.state.stations.map(function(nested,indexI) {
-      return <tr>{nested.map(function(name,indexJ) {
-        return <td key={indexJ} onClick={() => something(name,indexI,indexJ)}>{name}</td>;
-      })}
-      </tr>
-    });
-
-    var single = ['1','2','3'];
-    var singles = [
-      ['1','2','3'],
-      ['4','5','6']
-  ];
-
-
     return(
       <div>
-        <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
-          <Tab eventKey={1} title="Input">
-            This is Input
-          </Tab>
-          <Tab eventKey={2} title="Output">
 
-          <Table striped bordered condensed hover><tbody>
-            {users}
-          </tbody></Table>
-
-
-          </Tab>
-        </Tabs>
       </div>
     );
   }
