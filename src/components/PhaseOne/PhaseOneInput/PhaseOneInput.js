82b7eb24093316386268d55dc904c60e0cfa5acd
natalieblodgett <natalieblodgett@u.boisestate.edu>
2017-07-31 10:24:47 -0600

    Help popover text

--- src/components/PhaseOne/PhaseOneInput/PhaseOneInput.js
+++ src/components/PhaseOne/PhaseOneInput/PhaseOneInput.js
@@ -222,22 +222,19 @@ const Help = ({match}) =>(
 
     <Modal.Body>
       <h4>Requirements Table</h4>
-        <p>Specify the requirements for environmentally responsible design for your product below. Requirements must be equal in number and relevant to your functions.
+        <p>Specify requirements for environmentally responsible drone design below. Requirements must be equal in number and relevant to functions.<OverlayTrigger overlay={
+          <Popover
+            id="popover-basic"
+            placement="right"
+            positionLeft={200}
+            positionTop={50}
+            title="Popover right"
+            >
+            This is a Popover
+          </Popover>
+        }><a href="#">popover</a></OverlayTrigger>
+           here
         </p>
     </Modal.Body>
   </div>
 )
-
-
-/*<OverlayTrigger overlay={
-  <Popover
-    id="popover-basic"
-    placement="right"
-    positionLeft={200}
-    positionTop={50}
-    title="Popover right"
-    >
-    This is a Popover
-  </Popover>
-}><a href="#">popover</a></OverlayTrigger>
-   here*/
