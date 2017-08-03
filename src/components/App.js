377532c8bcaf492f9711464d5fc632498de461be
Masashi Schafer <masashi@virtualspaces.io>
2017-08-03 11:08:13 -0500

    Added AR

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import Resources from './Resources/Resources.js';
 import Project from './Project/Project.js';
 import View from './View/View.js';
 import Login from './Login/Login.js';
+import AR from './AR/AR.js';
 
 import TopNav from './SideBar/navbar.js';
 import {PageHeader,Grid} from 'react-bootstrap';
@@ -31,6 +32,7 @@ const App = () => (
         <Route path="/Login" component={Login}/>
         <Route path="/SPASS/About" component={SPASS}/>
         <Route path="/Dashboard" component={Dashboard}/>
+        <Route path ="/AR" component={AR}/>
 
         <Route path="/Phases" component={Phases}/>
       </div>
