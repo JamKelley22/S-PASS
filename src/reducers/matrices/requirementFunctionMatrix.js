fdd1ad9cdf45e81bb1150ff8f89362425523a5b3
Jameel <jkelley@iastate.edu>
2017-08-31 01:18:08 -0500

    fixed bug kijung was having with requirement list not being fully displayed in other matrixes

--- src/reducers/matrices/requirementFunctionMatrix.js
+++ src/reducers/matrices/requirementFunctionMatrix.js
@@ -3,6 +3,9 @@ const initialState = math.matrix([
   [0.4, 0.2, 0.1, 0, 0, 0, 0 ,0],
   [0, 0, 0, 0.6, 0, 0, 0 ,0],
   [0, 0.1, 0, 0, 0.4, 0, 0 ,0],
+  [0, 0.1, 0, 0, 0.4, 0, 0 ,0],
+  [0, 0.1, 0, 0, 0.4, 0, 0 ,0],
+  [0, 0.1, 0, 0, 0.4, 0, 0 ,0],
   ]);
 
 export default function(state = initialState, action){
