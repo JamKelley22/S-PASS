c2ee502c15145af06ad571dbe948b48b111de6a0
natalieblodgett <natalieblodgett@u.boisestate.edu>
2017-09-13 10:30:18 -0600

    About S-PASS page

--- src/components/Project/About.js
+++ src/components/Project/About.js
@@ -10,22 +10,17 @@ const About = ({match}) =>(
     <div id='top'>
       <h1>About the S-PASS Tool</h1>
       <p>
-      Sustainable Product Architecture and Supplier Selection (S-PASS) The Sustainable Product Architecture and Supplier Selection (S-PASS) tool, designed to guide users to identify sustainable product architectures and their suppliers. The use of S-PASS within this platform aims to:
+      The Sustainable Product Architecture and Supplier Selection (S-PASS) tool is designed to guide users in identifying sustainable product architectures and related suppliers. The use of S-PASS within this platform aims to enhance students’ class activities relevant to sustainable product and service design modules, and to provide an easy to use and effective tool for determining product architectures and Original Equipment Manufacturer (OEM) suppliers with consideration of environmental impacts.
       </p>
       <p>
-      <b>1. </b>Enhance students’ class activities relevant to sustainable product and service design modules, and
+      An overview of S-PASS is illustrated in the figure below. The tool employs a matrix propagation system which constructs and uses a series of overlapping matrices to derive a final solution. Users input information regarding sustainability requirements, existing and alternative modules, and new suppliers. This information is then processed according to thresholds on requirement satisfaction and environmental impact to reveal acceptable alternative modules (product architectures) and related suppliers.
       </p>
-      <p>
-      <b>2. </b>Provide an easy to use and effective tool to enable students to determine product architectures and Original Equipment Manufacturer (OEM) suppliers with consideration of possible environmental impacts.
-      </p>
-      <p>
-      An overview of S-PASS is illustrated in the figure below. The S-PASS basically employs a matrix propagation system, which constructs and uses a series of overlapping matrices, to derive a final solution through matrix operations starting from the rows in the initial matrix to columns in the last matrix.
-      </p>
+
 
       <Image src={require('../../Images/S-PASS_Diagram.PNG')} alt='S-PASS Diagram'/>
 
       <p>
-      Students’ information input regarding new part modules and suppliers in consideration of sustainable design requirements and proper environmental impact is processed through the matrix system to obtain acceptable sustainable product architectures and their suppliers.
+      The S-PASS Tool is comprised of three phases. In each phase, there is an input section where the user must input information, and an output section where the user can evaluate results.
       </p>
     </div>
 
