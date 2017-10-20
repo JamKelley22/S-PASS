cb2436b21f96440c55ccaa6afa44a53460d12262
natalieblodgett <natalieblodgett@u.boisestate.edu>
2017-10-19 20:26:04 -0600

    Text from Excel

--- src/components/Project/About.js
+++ src/components/Project/About.js
@@ -8,9 +8,34 @@ import './Project.css'
 const About = ({match}) =>(
   <div>
     <div id='top'>
-      <h1>About the S-PASS Tool</h1>
+
+
+    <h1>Learning Objectives</h1>
+
+    <p>
+    Learning Objectives
+    Students completing this case study will identify the challenges and benefits of sustainability decision making for design and manufacturing. The learning objectives of this case study are listed as follows:
+    </p>
+    <p>
+    •	Students will use multi-perspectives for product design, considering requirements, functions, modules, product architectures, and suppliers, to develop environmentally responsible products.
+    </p>
+    <p>
+    •	Students will perform a design decision-making process to integrate environmental issues into selection of product concepts and suppliers through the S-PASS (Sustainable Product Architecture and Supplier Selection) tool.
+    </p>
+    <p>
+    •	Students will formulate their own design problems for development of environmentally responsible products.
+    </p>
+
+
+      <h1>Case Study</h1>
       <p>
-      The Sustainable Product Architecture and Supplier Selection (S-PASS) tool is designed to guide users in identifying sustainable product architectures and related suppliers. The use of S-PASS within this platform aims to enhance students’ class activities relevant to sustainable product and service design modules, and to provide an easy to use and effective tool for determining product architectures and Original Equipment Manufacturer (OEM) suppliers with consideration of environmental impacts.
+      Drones and multi-copters are very familiar to consumers. While children play with remote controlled toy versions, some adults expect deliveries by drones. Company X is currently selling two types of drones: hexa-copters and quad-copters (See Table 1). Due to global regulations regarding the environmental impacts of products and companies, Company X is planning to upgrade its existing products through environmentally responsible design.
+      </p>
+      <p>
+      The main objective of environmentally responsible drone design at Company X is to determine new product architectures and their suppliers while considering their possible environmental impacts. New drones should satisfy design requirements for minimal energy use and reduction in hazardous by-products/pollutants both from the manufacturing process and throughout useful life of drones. Modules in new drones should be provided by suppliers that are environmentally friendly in their operations (i.e., manufacturing and logistics). Key modules and components of the existing drone designs are provided in Table 1.
+      </p>
+      <p>
+Company X has just signed a consulting agreement with your team to tap into your expertise in sustainable design and manufacturing.  You are tasked with proposing new designs that are vastly improved in environmental sustainability performance over the current designs of drones. For facilitating the design task, your team will employ the Sustainable Product Architecture and Supplier Selection (S-PASS) tool provided in class (See Appendix). As a team, follow each phase outlined below and determine the input values to use within the S-PASS tool.
       </p>
       <p>
       An overview of S-PASS is illustrated in the figure below. The tool employs a matrix propagation system which constructs and uses a series of overlapping matrices to derive a final solution. Users input information regarding sustainability requirements, existing and alternative modules, and new suppliers. This information is then processed according to thresholds on requirement satisfaction and environmental impact to reveal acceptable alternative modules (product architectures) and related suppliers.
@@ -19,9 +44,6 @@ const About = ({match}) =>(
 
       <Image src={require('../../Images/S-PASS_Diagram.PNG')} alt='S-PASS Diagram'/>
 
-      <p>
-      The S-PASS Tool is comprised of three phases. In each phase, there is an input section where the user must input information, and an output section where the user can evaluate results.
-      </p>
     </div>
 
     <div id='bottom'>
