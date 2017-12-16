517d1b8db07d332aac64569990d8fc1c5d169d56
Jameel <jkelley@iastate.edu>
2017-12-15 18:41:33 -0600

    test

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('*',function(req,res){//new
   res.sendFile(path.join(__dirname+'/www/index.html'));
 });
 
-const server = app.listen(3000, function() {
+const server = app.listen(process.env.PORT || 3000, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
